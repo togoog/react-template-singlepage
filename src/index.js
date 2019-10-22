b641597cd1a91c34c1d7aafdb902638951d665b0
heshiyu <450979309@qq.com>
2019-10-22 20:05:54 +0800

    feature: Import global.less into index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Page from './Page';
 import 'core-js'; // 兼容IE处理
 import '@/assets/iconfont/iconfont.css';
+import '@/assets/css/index.less';
 
 const render = Component => ReactDOM.render(<Component />, document.getElementById('root'));
 
